26f1dc7f1f6fb63e718007991a9e2b4d5c43a6b3
botirbek.kulzhanov <botirbek.kulzhanov@fabrique.studio>
2023-04-03 13:43:08 +0600

    Added README file

--- test_pdf.py
+++ test_pdf.py
@@ -1,8 +1,12 @@
 import pytest
 
 def test_valid_pdf(get_word_dictionary):
+    print('Expected result \n')
+
     ethalon_file=get_word_dictionary('test_task.pdf')
     print(ethalon_file)
+
+    print('\n Actual result \n')
 
     test_file=get_word_dictionary('test_task.pdf')
     print(test_file)
@@ -14,9 +18,13 @@ def test_valid_pdf(get_word_dictionary):
 
 
 def test_invalid_pdf(get_word_dictionary):
+    print('Expected result \n')
+    
     ethalon_file=get_word_dictionary('test_task.pdf')
     print(ethalon_file)
 
+    print('\n Actual result \n')
+    
     test_file=get_word_dictionary('TestPDFfile.pdf')
     print(test_file)
 
